dc4088a6624caf7bcd6189c3ed3f56fc4f512088
Moracras <tavfikkarademir@gmail.com>
2023-11-21 21:01:02 +0300

    cryptosearch

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-getCoins = () =>{
+const API_KEY = 'coinrankingaf5d04619b7d8af1a08b3ff829ad7f04f171d48bc4bd629a'
+const getCoins = () =>{
     const options = {
         headers: {
           'x-access-token': 'coinrankingaf5d04619b7d8af1a08b3ff829ad7f04f171d48bc4bd629a',
@@ -7,5 +8,22 @@ getCoins = () =>{
       
       fetch('https://api.coinranking.com/v2/coins', options)
         .then((response) => response.json())
-        .then((result) => console.log(result));
+        .then((result) => console.log(result.data.coins));
 }
+
+getCoins()
+
+const searchCrypto = (search) =>{
+  const options = {
+    headers: {
+      'x-access-token': API_KEY,
+    },
+  };
+  
+  fetch(`https://api.coinranking.com/v2/search-suggestions?query=${search}`, options)
+    .then((response) => response.json())
+    .then((result) => console.log(result));
+}
+
+
+searchCrypto("eth")
