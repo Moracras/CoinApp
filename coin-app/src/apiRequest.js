32ccc1426b85e7f5b8dbd5b06ea984ebb7a2547f
Moracras <tavfikkarademir@gmail.com>
2023-11-22 23:17:38 +0300

    viteapikey file

--- coin-app/src/apiRequest.js
+++ coin-app/src/apiRequest.js
@@ -1,8 +1,8 @@
 import { renderCoins } from "./renderData";
 export const apiRequest = async (input)=>{
     
-    const API_KEY = 'coinrankingaf5d04619b7d8af1a08b3ff829ad7f04f171d48bc4bd629a'
-    const options = {headers: {'x-access-token': API_KEY,},};
+    // const API_KEY = 'coinrankingaf5d04619b7d8af1a08b3ff829ad7f04f171d48bc4bd629a'
+    const options = {headers: {'x-access-token': import.meta.env.VITE_API_KEY,},};
   
     const url =`https://api.coinranking.com/v2/coins?search=${input}`
     try{
